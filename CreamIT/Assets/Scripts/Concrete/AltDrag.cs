e6b6b8933ff38b4d5d31cf512a8b3f6059d354c7
Anthony Romrell <anthony@artisandream.com>
2016-11-24 00:24:52 -0700

    updated dragging with an offest.

--- CreamIT/Assets/Scripts/Concrete/AltDrag.cs
+++ CreamIT/Assets/Scripts/Concrete/AltDrag.cs
@@ -7,6 +7,7 @@ public class AltDrag : MonoBehaviour
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
         {
             mousePosition = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+            mousePosition.z = 0;
         	transform.position = mousePosition;
         }
     }
--- CreamIT/Assets/Scripts/Concrete/DragObject.cs
+++ CreamIT/Assets/Scripts/Concrete/DragObject.cs
@@ -1,12 +1,22 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class DragObject : MonoBehaviour
 {
-    Vector3 mousePosition;
+    Vector3 screenPoint;
+    Vector3 offset;
+    Vector3 clickPosition;
+    Vector3 movePosition;
+    
+    void OnMouseDown()
+    {
+        screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
     void OnMouseDrag()
     {
-        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        transform.position = mousePosition;
+        clickPosition = Input.mousePosition;
+        movePosition = Camera.main.ScreenToWorldPoint(clickPosition) + offset;
+        transform.position = movePosition;
     }
 }
