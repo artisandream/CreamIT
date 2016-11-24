73ed37b1026ebbc6ce27b792f538308bf6a9182d
Anthony Romrell <anthony@artisandream.com>
2016-11-24 00:48:58 -0700

    Working on Triggering color.

---
+++ CreamIT/Assets/Scripts/Concrete/TempTrigger.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TempTrigger : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other)
+	{
+		print("h");
+		SpriteRenderer render = GetComponent<SpriteRenderer>();
+		render.color = SetColor.OnSetColor();
+	}
+}
