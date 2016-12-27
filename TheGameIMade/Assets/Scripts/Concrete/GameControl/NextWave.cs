2a7f5b86e4f6925e4f6f36d65d3588eeca504dbf
Anthony Romrell <anthony@artisandream.com>
2016-12-27 15:54:11 -0700

    Minor Refactor

--- TheGameIMade/Assets/Scripts/Concrete/GameControl/NextWave.cs
+++ TheGameIMade/Assets/Scripts/Concrete/GameControl/NextWave.cs
@@ -1,32 +1,39 @@
 ﻿using System;
 using UnityEngine;
 
-public class NextWave : MonoBehaviour {
+public class NextWave : MonoBehaviour
+{
 
-	public static Action GoToNextWave;
-	public static Action OnWinGame;
-	public int ringCount;
+    public static Action GoToNextWave;
+    public static Action OnWinGame;
+    public int ringCount;
 
-	void Start()
-	{
-		RunGame.OnStartWave += OnStartWaveHandler;
-	}
+    void Start()
+    {
+        RunGame.OnStartWave += OnStartWaveHandler;
+    }
 
     private void OnStartWaveHandler()
     {
-		ringCount = StaticFunctions.currentWave.ringCount;
+        ringCount = StaticFunctions.currentWave.ringCount;
     }
 
-	void OnTriggerEnter(Collider other)
-	{
-		ringCount--;
-		StaticFunctions.totalRingCount--;
-		if(StaticFunctions.totalRingCount == 0) {
-			OnWinGame();
-		}
+    void OnTriggerEnter(Collider other)
+    {
+        ringCount--;
+        StaticFunctions.totalRingCount--;
+        if (StaticFunctions.totalRingCount == 0)
+        {
+            OnWinGame();
+        }
 
-		if(ringCount == 0) {
-			GoToNextWave();
-		}
-	}
+        if (ringCount == 0)
+        {
+            if (GoToNextWave != null)
+            {
+                GoToNextWave();
+            }
+
+        }
+    }
 }
