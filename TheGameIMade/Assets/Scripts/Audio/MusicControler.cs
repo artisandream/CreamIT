b3830ca70a7381c75437988c611eadaf1bdaa1a2
Anthony Romrell <anthony@artisandream.com>
2016-12-27 17:59:43 -0700

    Added Audio Files.

---
+++ TheGameIMade/Assets/Scripts/Audio/MusicControler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MusicControler : MonoBehaviour {
+
+	public AudioMixerSnapshot inActionColor;
+	public AudioMixerSnapshot outActionColor;
+	public float fadeIn;
+	public float fadeOut;
+
+	public void ColorActionStart()
+	{
+		inActionColor.TransitionTo(fadeIn);
+	}
+
+	public void ColorActionStop()
+	{
+		outActionColor.TransitionTo(fadeOut);
+	}
+}
