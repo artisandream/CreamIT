7def839be26f853d72750bacdd41e31e65cbacd9
Greg Alger <galger10@hotmail.com>
2017-12-02 13:46:37 -0800

    Adding Spawn files

---
+++ TheGameIMade/Assets/Scripts/Spawning/GamePiece.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class GamePiece : MonoBehaviour
+{
+    [SerializeField] private float _speed = 2.0f;
+    private int _currentWayPoint = 0;
+    private List<WayPoint> _wayPoints = new List<WayPoint>();
+
+    public void AddWayPoint(WayPoint newWayPoint)
+    {
+        _wayPoints.Add(newWayPoint);
+    }
+
+    public void MoveNextWayPoint()
+    {
+        //TODO: Fix so piece can only hit one way point per row
+        //TODO: For now added skip
+        if (_currentWayPoint >= _wayPoints.Count)
+        {
+            return;
+        }
+
+        gameObject.transform.DOMove(_wayPoints[_currentWayPoint].GetPosition(), _speed);
+        _currentWayPoint++;
+    }
+}
---
+++ TheGameIMade/Assets/Scripts/Spawning/SpawnManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+	private List<WayPointRow> _wayPointRows = new List<WayPointRow>();
+	[SerializeField] private GameObject _startPoint;
+	[SerializeField] private WayPoint _endWayPoint;
+	[SerializeField] private GameObject _gamePiecePrefab;
+
+	private void Awake()
+	{
+		WayPointRow.WayPointRowAdded += WayPointRowAdded;
+
+		StartCoroutine(SpawnNewWord());
+	}
+
+	private IEnumerator SpawnNewWord()
+	{
+		while (true)
+		{
+			yield return new WaitForSeconds(2.5f);
+			
+			var wordCount = Random.Range(3, 7);
+			Debug.Log("SpawnNewWord: " + wordCount);
+			StartCoroutine(SpawnNewPiece(wordCount));
+		}
+	}
+
+	private IEnumerator SpawnNewPiece(int wordLength)
+	{
+		while (wordLength > 0)
+		{
+			yield return new WaitForSeconds(0.5f);
+					
+			var newGamePieceObject = Instantiate(_gamePiecePrefab, _startPoint.transform.position, Quaternion.identity, _startPoint.transform);
+			var newGamePiece = newGamePieceObject.AddComponent<GamePiece>();
+			
+			foreach (var wayPointRow in _wayPointRows)
+			{
+				var wayPoint = wayPointRow.GetRandomWayPoint();
+				newGamePiece.AddWayPoint(wayPoint);
+			}
+			
+			newGamePiece.AddWayPoint(_endWayPoint);
+			
+			newGamePiece.MoveNextWayPoint();
+
+			wordLength--;
+		}
+	}
+
+	private void OnDestroy()
+	{
+		WayPointRow.WayPointRowAdded -= WayPointRowAdded;
+	}
+
+	private void WayPointRowAdded(WayPointRow newWayPointRow)
+	{
+		_wayPointRows.Add(newWayPointRow);
+	}
+	
+	
+
+}
