a71b9b48e5dfc9455301de5359fb1bcd544ffe59
Anthony Romrell <anthony@artisandream.com>
2017-12-02 15:20:56 -0700

    Added S Objects

---
+++ TheGameIMade/Assets/Scripts/ScriptableObjects/WordData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+
+public class WordData : ScriptableObject
+{
+
+	public Characters AvaliableCharacters;
+	public List<Word> Words;
+
+
+}
