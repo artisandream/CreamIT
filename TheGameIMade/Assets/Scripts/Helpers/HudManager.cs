dcb9b4be32cb5926401e5d3a452665b1fa0be731
Greg Alger <galger10@hotmail.com>
2017-12-09 09:11:43 -0800

    Adding Files

---
+++ TheGameIMade/Assets/Scripts/Helpers/HudManager.cs
@@ -0,0 +1,25 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HudManager : MonoBehaviour
+{
+	[SerializeField] private Text _wordDefinitionText;
+
+	// Use this for initialization
+	private void Start()
+	{
+		SpawnManager.DefinitionChanged += DefinitionChanged;
+	}
+
+	private void DefinitionChanged(string newDefinition)
+	{
+		_wordDefinitionText.DOText(newDefinition, 0.75f, false, ScrambleMode.Lowercase, newDefinition);
+	}
+
+	// Update is called once per frame
+	private void OnDestroy()
+	{
+		SpawnManager.DefinitionChanged -= DefinitionChanged;
+	}
+}
