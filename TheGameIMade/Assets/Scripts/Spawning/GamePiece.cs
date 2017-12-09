780c5135ca7706870e97aa219b7222c71e118759
Greg Alger <galger10@hotmail.com>
2017-12-09 14:03:56 -0800

    Adding Drag and Drop Support

--- TheGameIMade/Assets/Scripts/Spawning/GamePiece.cs
+++ TheGameIMade/Assets/Scripts/Spawning/GamePiece.cs
@@ -4,25 +4,5 @@ using UnityEngine;
 
 public class GamePiece : MonoBehaviour
 {
-    [SerializeField] private float _speed = 2.0f;
-    private int _currentWayPoint = 0;
-    private List<WayPoint> _wayPoints = new List<WayPoint>();
-
-    public void AddWayPoint(WayPoint newWayPoint)
-    {
-        _wayPoints.Add(newWayPoint);
-    }
-
-    public void MoveNextWayPoint()
-    {
-        //TODO: Fix so piece can only hit one way point per row
-        //TODO: For now added skip
-        if (_currentWayPoint >= _wayPoints.Count)
-        {
-            return;
-        }
-
-        gameObject.transform.DOMove(_wayPoints[_currentWayPoint].GetPosition(), _speed);
-        _currentWayPoint++;
-    }
+   public char CharecterValue = 'a';
 }
---
+++ TheGameIMade/Assets/Scripts/Spawning/LetterGamePiece.cs
@@ -0,0 +1,55 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class LetterGamePiece : GamePiece, 
+    IBeginDragHandler, 
+    IDragHandler, 
+    IEndDragHandler
+{
+    [SerializeField] private float _moveSpeed = 0.75f;
+    private Vector3 _startPosition;
+    [HideInInspector] public bool IsMatched = false;
+    [HideInInspector] public EmptyLetterGamePiece LastCollidedEmptyPiece = null;
+    
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        _startPosition = this.transform.position;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (eventData.pointerEnter != null && eventData.dragging)
+        {
+            this.transform.position = eventData.position;
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        if (!AttachLetter())
+        {
+            this.gameObject.transform.DOMove(_startPosition, _moveSpeed);
+        }
+    }
+
+    private bool AttachLetter()
+    {
+        if (LastCollidedEmptyPiece == null || LastCollidedEmptyPiece.CharecterValue != this.CharecterValue)
+        {
+            return false;
+        }
+
+        if (IsMatched)
+        {
+            return true;
+        }
+
+        IsMatched = true;
+
+        this.transform.SetParent(LastCollidedEmptyPiece.gameObject.transform);
+
+        return true;
+    }
+
+}
